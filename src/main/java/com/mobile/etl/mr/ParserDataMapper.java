7971d2cb1efe722d2c7e4e0cb07cb1617c615356
dongge <775600040@qq.com>
2018-10-24 15:06:37 +0800

    parser data util

---
+++ src/main/java/com/mobile/etl/mr/ParserDataMapper.java
@@ -0,0 +1,11 @@
+package com.mobile.etl.mr;
+
+/**
+ * @ClassName ParserDataMapper
+ * @Author lyd
+ * @Date $ $
+ * @Vesion 1.0
+ * @Description 将hdfs中清洗的数据存储到hdfs中
+ **/
+public class ParserDataMapper {
+}
---
+++ src/main/java/com/mobile/etl/util/LogParserUtil.java
@@ -0,0 +1,11 @@
+package com.mobile.etl.util;
+
+/**
+ * @ClassName LogParserUtil
+ * @Author lyd
+ * @Date $ $
+ * @Vesion 1.0
+ * @Description //TODO $
+ **/
+public class LogParserUtil {
+}
